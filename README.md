b8f97fd9bfdedef644a46ca4350244f176d8dc53
ritaurbani <ritaurbani@gmail.com>
2025-04-12 19:37:06 +0200

    c

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
-# React + Vite
+Consegna
+Repo: ex-react-usecallback-autocomplete
+💡 Premessa: Stai sviluppando un campo di ricerca intelligente simile a quello di Amazon. Quando l'utente digita, una tendina di suggerimenti mostra i prodotti corrispondenti alla ricerca. Per evitare richieste API eccessive, devi ottimizzare la ricerca con il debounce.
+Nota: a differenza di quanto visto finora negli esempi, per accedere all'API utilizzare utilizzare l'url base:
+https://boolean-spec-frontend.vercel.app/freetestapi
+al posto di:
+https://freetestapi.com/api/v1
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Ad esempio:
+https://boolean-spec-frontend.vercel.app/freetestapi/users
+per chiamare l'endpoint /users
+📌 Milestone 1: Creare un campo di ricerca e mostrare la lista dei suggerimenti
+Crea un campo di input (<input type="text">) in cui l’utente può digitare.
 
-Currently, two official plugins are available:
+Effettua una chiamata API a: 
+https://boolean-spec-frontend.vercel.app/freetestapi/products?search=[query]
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+La query deve essere sostituita con il testo digitato.
+Mostra i risultati API sotto l'input in una tendina di suggerimenti.
 
-## Expanding the ESLint configuration
+Se l'utente cancella il testo, la tendina scompare.
 
-If you are developing a production application, we recommend using TypeScript and enable type-aware lint rules. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+Obiettivo: Mostrare suggerimenti dinamici in base alla ricerca dell'utente.
+
+📌 Milestone 2: Implementare il Debounce per Ottimizzare la Ricerca
+Attualmente, ogni pressione di tasto esegue una richiesta API. Questo è inefficiente!
+Implementa una funzione di debounce per ritardare la chiamata API fino a quando l’utente smette di digitare per un breve periodo (es. 300ms)
+Dopo l’implementazione, verifica che la ricerca non venga eseguita immediatamente a ogni tasto premuto, ma solo dopo una breve pausa.
+
+Obiettivo: Ridurre il numero di richieste API e migliorare le prestazioni.
+
+🎯 Bonus: Caricare i Dettagli del Prodotto Selezionato
+Quando l’utente clicca su un prodotto nella tendina, nascondi la tendina e carica i dettagli completi del prodotto sotto il campo di ricerca.
+
+Effettua una richiesta API per ottenere i dettagli completi:
+https://boolean-spec-frontend.vercel.app/freetestapi/products/{id}
+
+Mostra i dettagli del prodotto selezionato (es. image, name, description, price).
+
+
+Obiettivo: Aggiungere interattività permettendo di visualizzare le informazioni complete di un prodotto.
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react'
 import './App.css'
-
+//import {debaounce} from "lodash"   //nmp install lodash
 function debounce(callback, delay){
   let timer;
   return(value) => {
@@ -15,6 +15,8 @@ function App() {
   const [query, setQuery] = useState('')
   const [options, setOptions] = useState([])
   const [error, setError] = useState(null)
+  const [selectedProduct, setSelectedProduct] = useState(null)//devo stampare questo prodotto da API grab it so state
+//all inizio non ho selezionato nulla
 
   //ritardare la chiamata API fino a quando 
   //l’utente smette di digitare per un breve periodo (es. 300ms)
@@ -22,10 +24,11 @@ function App() {
   const getQuery =   useCallback(debounce( 
     async (query) => {//Senza questo controllo, una query con soli spazi (" ") triggererebbe una fetch inutile.
      //VALIDAZIONE INPUT
-      if (!query.trim()) return; // Blocca ricerche inutili con query vuote o solo spazi.
+      if (!query.trim())  // Blocca ricerche inutili con query vuote o solo spazi.
       //Prima di ogni fetch basata su input utente
-      // setOptions([])
-
+      {// setOptions([])//uguale a dire query.trim()===0
+        return;
+      }//non fare fetch
     try {
       console.log("query:", query)
       const resp = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`)
@@ -38,15 +41,39 @@ function App() {
     }
   }, 300), []) 
 
-  //al cambio di query prelevo i film che rispondono alla query(value= query)
-  useEffect(() => {
+  //questa chiamata verra fatta solo quando non la ripetiamo per 500 sec di fila
+  //const debouncedGetQuery = useCallback(debounce(getQuery, 300), [])//funzione da salvare ritornata da debounce, dipendeza vuota, creare funzione una volta sola
+  
+  1.
+  //al cambio di query quindi ogni volta che scrivo..prelevo i prodotti che rispondono alla query(value= query)
+  //una funziona aggiorna l array di options > 
+  // fai un operazione ad ogni cambio dello stato => USEEFFET
+  useEffect(() => {//in useEffect mai mettere async await
     getQuery(query)
-  }, [query])
-
+  }, [query])//controllo se query e vuota(non e stato cercato nessuno prodotto) perche altrimenti non ha senso fare chiamata
+// fetch(..)
+//.then(res => res.json())
+//.then(data => setOptions(data))
+//.catch(error => console.error(error))
 
   const handleChange = (e) => {
     setQuery(e.target.value)
   }
+
+//raccogliere dettagli complati - debounce not needed
+   const fetchProductDetails = async (id) => {
+    try {
+      //salviamo il resolve di questa promise
+      const res = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products/${id}`)
+      const result = await res.json()
+      setSelectedProduct(result)
+      setQuery("")
+      setOptions([])//quando chiamata a buon fine nascondere tendina(svuoto)
+    }catch(error){
+      console.error(error)
+    }
+ 
+   }
 
   return (
     <>
@@ -61,14 +88,16 @@ function App() {
       <div>
 
         <ul>
-          {options.length > 0 && query && (//poiche ho questo quando cancello query si cancellano anche risultati setOptions([])
-            options.map((item) => (
+          {options.length > 0 && // query && 
+          //poiche ho questo quando cancello query si cancellano anche risultati > setOptions([])
+            (<div className='dropdown'>
+           { options.map((item) => (
               //checks query once before mapping (more efficient)
               // query && like this is less efficient map still runs even when query is empty (wasted computation)
               //map still runs even when query is empty (wasted computation)
-              <li key={item.id}>{item.name}</li>
-            ))
-          )}
+              <li onClick={() => fetchProductDetails(item.id)}   key={item.id}>{item.name}</li>
+            ))}
+            </div>)}
         </ul>
         {/* Show "no match found" when there's a query but no results */}
         {query && options.length === 0 && (//quando cerco qualcosa e non c e risultato array vuoto []
@@ -80,6 +109,15 @@ function App() {
 
         {error && (
           <p>Error loading results</p>
+        )}
+
+        {selectedProduct && (
+          <div className='card'>
+            <h2>{selectedProduct.name}</h2>
+            <img src={selectedProduct.image} alt="" />
+            <p>{selectedProduct.description}</p>
+            <p><strong>Prezzo:</strong>{selectedProduct.price}$</p>
+          </div>
         )}
 
         {/* <ul>
